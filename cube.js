7fb1283eefc6a1f4d42f46d6f75e5ab13c766a04
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-06-14 11:35:37 -0500

    Now Checks if a row is complete

--- cube.js
+++ cube.js
@@ -4,6 +4,7 @@ function createCube(){
   c = new Clickable(1000,360);
   c.resize(60,60);
   c.color = "green";
+  c.cornerRadius = 0;
   c.text = "";
   c.onPress = function(){
     c.isPressed = true;    
@@ -13,7 +14,14 @@ function createCube(){
     checkCube();
     c.x = Math.floor(c.x / 30)*30;
     c.y = Math.floor(c.y / 30)*30;
+    let thisRow = c.y / 30 - 1
     placeCube();
+    if(isRowComplete(thisRow)){
+      changeRowColor(thisRow)
+    }
+    if(isRowComplete(thisRow+1)){
+      changeRowColor(thisRow+1)
+    }
     checkForThree();
   }
 }//end createCube
@@ -42,6 +50,9 @@ function placeCube(){
   let thisX = c.x / 30 - 1;
   let thisY = c.y / 30 - 1;
   g[thisX][thisY].color = c.color;
+  g[thisX+1][thisY].color = c.color;
+  g[thisX+1][thisY+1].color = c.color;
+  g[thisX][thisY+1].color = c.color;
   c.x = 600
   
 }
